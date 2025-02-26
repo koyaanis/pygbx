49a65ccd0e50ac63e17c0430eaa8a9131e0526f1
Lars Matthies <qkoyaanis@gmail.com>
2025-02-26 15:48:33 +0100

    Add Quaternion to __init__.py

--- pygbx/__init__.py
+++ pygbx/__init__.py
@@ -1,4 +1,4 @@
-from pygbx.headers import CGameHeader, CGameCtnCollectorList, CollectorStock, MapBlock, Vector3, CGameChallenge, CGameBlockItem, CGameWaypointSpecialProperty, CGameCommon, CGameReplayRecord, CGameGhost, CGameCtnGhost, ControlEntry, GhostSampleRecord
+from pygbx.headers import CGameHeader, CGameCtnCollectorList, CollectorStock, MapBlock, Vector3, Quaternion, CGameChallenge, CGameBlockItem, CGameWaypointSpecialProperty, CGameCommon, CGameReplayRecord, CGameGhost, CGameCtnGhost, ControlEntry, GhostSampleRecord
 from pygbx.bytereader import ByteReader
 from pygbx.stadium_blocks import STADIUM_BLOCKS
 from pygbx.canyon_blocks import CANYON_BLOCKS
